37691f1107f99acd1d65fba3743f88683dce83f7
Michael N. Gagnon <mng@Michaels-MBP.san.rr.com>
2020-10-15 12:35:39 -0700

    .

--- run-php.sh
+++ run-php.sh
@@ -3,19 +3,11 @@
 path=$1
 rootpath=$2
 
-#echo "rootpath = $rootpath"
-
 totaldepth=`awk -F"/" '{print NF-1}' <<< "$path"`
 rootdepth=`awk -F"/" '{print NF-1}' <<< "$rootpath"`
 reldepth=`expr $totaldepth - $rootdepth`
-
-#echo "rel depth = $reldepth for $path"
-
 dir=`dirname "$path"`
 relpath=`grealpath --relative-to="$rootpath" "$dir"`
-
-
-echo dir "$dir"
 
 cp "index.php" "$dir"
 cd "$dir"

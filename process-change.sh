7849653ff90d8c9ed2776774347532ef20c84248
Michael N. Gagnon <mng@Michaels-MBP.san.rr.com>
2020-10-15 22:41:02 -0700

    .

--- process-change.sh
+++ process-change.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 
 path=$1
 trash=$2
@@ -6,6 +7,29 @@ rootpath=$3
 
 if [[ -f  "$path" ]]
 then
+
+ #	dirpath=`dirname $path`
+
+
+ #    #cp touch.sh.template $path/touch.template
+ #    echo "."
+ #    echo "."
+ #    echo "."
+ #    echo "."
+ #    echo "."
+ #    echo "."
+
+ #    cat touch.sh.template | sed s+DEST+$dirpath+g > touch.sh
+ #    #mv touch.sh $path/touch.sh
+	# echo "."
+ #    echo "."
+ #    echo "."
+ #    echo "."
+ #    echo "."
+ #    echo "."
+
+
+
     #echo "$path file exists"
     ./process-file-change.sh "$path" "$trash" "$rootpath"
 else
@@ -18,6 +42,23 @@ else
 	    # echo "rootpath: $rootpath"
 	    bname="$(basename $path)"
 
+
+	 #    #cp touch.sh.template $path/touch.template
+	 #    echo "."
+	 #    echo "."
+	 #    echo "."
+	 #    echo "."
+	 #    echo "."
+	 #    echo "."
+
+	 #    cat touch.sh.template | sed s+DEST+$path+g > touch.sh
+	 #    #mv touch.sh $path/touch.sh
+		# echo "."
+	 #    echo "."
+	 #    echo "."
+	 #    echo "."
+	 #    echo "."
+	 #    echo "."
 	    
 
 	    if [ "$bname" = "x" ]; then
---
+++ touch.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd /Users/mng/Desktop/rabbit-holes/old-town/main
+pwd
+#ls -1 *.jpg | xargs -I{} touch {}

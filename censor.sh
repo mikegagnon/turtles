65ca5321e831628d9298594ffda016ac8727219a
Michael N. Gagnon <mng@Michaels-MBP.san.rr.com>
2020-10-17 10:06:30 -0700

    rm dstore

--- censor.sh
+++ censor.sh
@@ -6,6 +6,7 @@ destpath="$rootpath-censored"
 if ./is-censor-safe.sh "$rootpath"; then
 	cp -r "$rootpath" "$destpath"
 	./convert-z.sh "$destpath"
+	find "$destpath" | grep ".DS_Store$" | xargs -I{} rm {}
 else
 	echo "Aborting ./censor.sh"
 	exit 1
